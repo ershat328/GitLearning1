e39c7a25739bd0d61447dcb0d605dd8a7cfd5808
ershat328 <h-abd@mail.com>
2020-05-12 04:21:47 +0200

    update Login, HelloGitWorld.java

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -3,5 +3,7 @@ package tests;
 public class Login {
     public static void main(String[] args) {
         System.out.println ("inshaalllah" );
+
+        System.out.println ("i will do it" );
     }
 }
