130ecee83291df5f0b406d83301c0263a363d9d9
ershat328 <57468265+ershat328@users.noreply.github.com>
2020-05-19 03:51:16 +0200

    Update practice.java

--- src/tests/practice.java
+++ src/tests/practice.java
@@ -4,5 +4,9 @@ public class practice {
     public static void main(String[] args) {
 
         System.out.println ("practice will make u master" );
+        
+        // i will se this line on intellij
+        
+        //i added this line form github
     }
 }
